64ba7886dabcc59a9b2227ed4d0a0040967fb55a
Sigureya <sigureya1208@gmail.com>
2025-04-28 04:23:10 +0900

    ギリギリ動く状態に修正できた。この後でコメントアウト箇所を順次復旧する

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,58 @@
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-//import tseslint from "typescript-eslint";
+// import fn from "eslint-plugin-functional";
+// import globals from "globals";
+// import reactHooks from "eslint-plugin-react-hooks";
+// import reactRefresh from "eslint-plugin-react-refresh";
+// import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
   { ignores: ["dist", "**/*.d.ts"] },
   {
-    //    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    //    files: ["src/**/*.ts", "vite.config.mts"],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      //      ecmaVersion: 2020,
+      //      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        // project: "./tsconfig.json", // 型情報を提供するためにプロジェクトを指定
+      },
     },
     plugins: {
       "@typescript-eslint": tseslint,
-
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      //    "@functional": fn,
+      // "react-hooks": reactHooks,
+      // "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
+      // ESLintの推奨ルールセットを手動で適用
+      "no-undef": "error",
+      "no-console": "warn",
+      eqeqeq: "error",
+      curly: "error",
+
+      // 純粋関数強制Rule
+      //      "@functional/no-return-void": "warn",
+      //      "@functional/no-let": "error",
+      //
+      // その他のルール
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-imports": [
         "warn",
-        { allowConstantExport: true },
+        {
+          prefer: "type-imports",
+        },
       ],
+      "spaced-comment": "error",
+      // "react-refresh/only-export-components": [
+      //   "warn",
+      //   { allowConstantExport: true },
+      // ],
     },
-  }
-);
+  },
+];
