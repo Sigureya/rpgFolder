3bf188c7ede18ef4ca6a4eb46f435ea6cd27836a
Sigureya <sigureya1208@gmail.com>
2025-04-28 04:44:49 +0900

    ファイル配置を修正。ライブラリ的に使い回すものを分離した

---
+++ src/libs/folder/folderClass/types.ts
@@ -0,0 +1,16 @@
+export interface FolderEntry {
+  indexHTML: boolean;
+  gameRmmzProject: boolean;
+}
+export interface DataFolder {
+  actors: string;
+  enemies: string;
+  items: string;
+  skills: string;
+  weapons: string;
+  armors: string;
+  classes: string;
+  states: string;
+  system: string;
+  commonEvents: string;
+}
