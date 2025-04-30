73716c981f1cf528a5f1e51d969ff7d5bd5fc9d0
Sigureya <sigureya1208@gmail.com>
2025-04-30 23:45:23 +0900

    関数担当に切り替えながら整理。消すかもしれない

--- src/libs/folder/folderClass/gameData/gameFolder.ts
+++ src/libs/folder/folderClass/gameData/gameFolder.ts
@@ -1,77 +1,19 @@
-import type { Dirent } from "node:fs";
-import {
-  FOLDER_DATA,
-  FILENAME_ACTORS,
-  FILENAME_ARMORS,
-  FILANAME_CLASSES,
-  FILENAME_ENEMIES,
-  FILENAME_ITEMS,
-  FILENAME_SKILLS,
-  FILENAME_WEAPONS,
-  FILENAME_TROOPS,
-  FILENAME_ANIMATIONS,
-  FILENAME_TILESET,
-  FILENAME_MAP_INFOS,
-  FILENAME_SYSTEM,
-} from "@sigureya/rpgtypes";
-import type { NodeLibs } from "@lib/nodeLib";
-const FOLDER_GAMES_ROOT = "./games" as const;
+import { FOLDER_DATA, FILENAME_ACTORS } from "@sigureya/rpgtypes";
+import type PathLib2 from "node:path";
 
-export const gameFolders = async (lib: NodeLibs): Promise<Dirent[]> => {
-  const list = await lib.fs.readdir(FOLDER_GAMES_ROOT, { withFileTypes: true });
-  return list.filter((dirent) => dirent.isDirectory());
+type PathLib_Resolve = Pick<typeof PathLib2, "resolve">;
+
+const filePath2 = (
+  lib: PathLib_Resolve,
+  baseFolder: string,
+  fileName: string
+) => {
+  return lib.resolve(baseFolder, FOLDER_DATA, fileName);
 };
 
-const filePath = (lib: NodeLibs, gameFolder: string, fileName: string) => {
-  return lib.path.join(FOLDER_GAMES_ROOT, gameFolder, FOLDER_DATA, fileName);
-};
-
-export const actorsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_ACTORS);
-};
-
-export const armorsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_ARMORS);
-};
-
-export const classesFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILANAME_CLASSES);
-};
-
-export const enemiesFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_ENEMIES);
-};
-
-export const itemsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_ITEMS);
-};
-
-export const skillsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_SKILLS);
-};
-
-export const weaponsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_WEAPONS);
-};
-
-export const troopsFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_TROOPS);
-};
-
-export const animationsFileName = (
-  lib: NodeLibs,
+export const actorsFilePath = (
+  lib: PathLib_Resolve,
   gameFolder: string
 ): string => {
-  return filePath(lib, gameFolder, FILENAME_ANIMATIONS);
-};
-
-export const systemFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_SYSTEM);
-};
-
-export const tilesetFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_TILESET);
-};
-export const mapInfosFileName = (lib: NodeLibs, gameFolder: string): string => {
-  return filePath(lib, gameFolder, FILENAME_MAP_INFOS);
+  return filePath2(lib, gameFolder, FILENAME_ACTORS);
 };
