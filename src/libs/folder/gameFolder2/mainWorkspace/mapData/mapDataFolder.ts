fd350ea21791d30667a9017ddc4efb8b43af3671
Sigureya <sigureya1208@gmail.com>
2025-05-01 17:37:15 +0900

    パス初期化を修正

--- src/libs/folder/gameFolder2/mainWorkspace/mapData/mapDataFolder.ts
+++ src/libs/folder/gameFolder2/mainWorkspace/mapData/mapDataFolder.ts
@@ -3,22 +3,22 @@ import { FOLDER_DATA, FILENAME_MAP_INFOS } from "@sigureya/rpgtypes";
 import type FsLib from "node:fs/promises";
 import type PathLib from "node:path";
 import type { ResultOfReadMapFile } from "./utils";
-import { mappingAllMapFiles } from "./utils";
+import { isMapFileName, mappingAllMapFiles } from "./utils";
+import type { Dirent } from "node:fs";
+import { ensureFolderPath } from "@lib/nodeLib";
 
 export class MapDataFolder {
+  public readonly basePath: string;
   constructor(
-    private pathLib: typeof PathLib,
-    private fsLib: typeof FsLib,
-    public readonly basePath: string,
-    public readonly folderName: string = FOLDER_DATA
-  ) {}
+    private pathLib: Pick<typeof PathLib, "join" | "sep" | "resolve">,
+    private fsLib: Pick<typeof FsLib, "readFile" | "readdir" | "writeFile">,
+    basePath: string
+  ) {
+    this.basePath = ensureFolderPath(pathLib, basePath, FOLDER_DATA);
+  }
 
   async readMapInfos(): Promise<Data_MapInfo[]> {
-    const filePath = this.pathLib.resolve(
-      this.basePath,
-      this.folderName,
-      FILENAME_MAP_INFOS
-    );
+    const filePath = this.pathLib.resolve(this.basePath, FILENAME_MAP_INFOS);
     const json = await this.fsLib.readFile(filePath, "utf-8");
     return JSON.parse(json) as Data_MapInfo[];
   }
@@ -46,5 +46,13 @@ export class MapDataFolder {
     const filePath = this.resolveMapFilePath(mapId);
     const json = await this.fsLib.readFile(filePath, "utf-8");
     return JSON.parse(json) as Data_Map;
+  }
+
+  async mapFiles(): Promise<Dirent[]> {
+    return (
+      await this.fsLib.readdir(this.basePath, {
+        withFileTypes: true,
+      })
+    ).filter((dirent) => dirent.isFile() && isMapFileName(dirent.name));
   }
 }
