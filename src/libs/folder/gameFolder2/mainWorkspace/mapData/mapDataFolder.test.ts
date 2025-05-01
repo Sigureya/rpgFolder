dc0838520a59ab759a7bc6b46e654597fccdd4cf
Sigureya <sigureya1208@gmail.com>
2025-05-02 04:35:58 +0900

    最小限のテストを追加

--- src/libs/folder/gameFolder2/mainWorkspace/mapData/mapDataFolder.test.ts
+++ src/libs/folder/gameFolder2/mainWorkspace/mapData/mapDataFolder.test.ts
@@ -1,13 +1,28 @@
-import { expect, describe, test } from "vitest";
+import { expect, describe, test, vi } from "vitest";
 import { MapDataFolder } from "./mapDataFolder";
-import { makeMockFs, makeMockPath } from "src/libs/testUtils";
 import pathLib from "node:path";
+import type { Dirent, PathLike } from "node:fs";
+
+interface FSX {
+  readFile: string;
+  readdir: Dirent[];
+}
+
+export const makeMockFs = (result: FSX) => ({
+  readFile: vi.fn((path: string, encode: string): Promise<string> => {
+    expect(encode).toBe("utf-8");
+    return Promise.resolve(result.readFile);
+  }),
+  readdir: vi.fn(() => Promise.resolve(result.readdir)),
+  writeFile: vi.fn((path: PathLike, data: string) => Promise.resolve()),
+});
+
 describe("MapDataFolder", () => {
   describe("constructor", () => {
+    const fsLib2 = makeMockFs({ readdir: [], readFile: "" });
+    const folder = new MapDataFolder(pathLib, fsLib2 as any, "basePath");
     test("should create an instance with the correct base path", () => {
-      const pathLib2 = makeMockPath({ resolve: "mockPath" });
-      const fsLib2 = makeMockFs({ readdir: [], readFile: "" });
-      const folder = new MapDataFolder(pathLib, fsLib2, "basePath");
+      expect(folder.basePath.endsWith("data")).toBe(true);
     });
   });
 });
