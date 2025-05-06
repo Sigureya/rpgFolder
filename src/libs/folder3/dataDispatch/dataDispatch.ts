bac6aaa7c05564d63d764ce5095d47ca25dde6ab
Sigureya <sigureya1208@gmail.com>
2025-05-06 23:30:01 +0900

    ツクールのデータを複数読み込み、関数で対応するための関数

---
+++ src/libs/folder3/dataDispatch/dataDispatch.ts
@@ -0,0 +1,157 @@
+import type PathLib from "node:path";
+import type FsLib from "node:fs/promises";
+import type { DataMapper } from "./types";
+import { ensureDataPath } from "src/libs/pathV2";
+import type { Data_CommonEvent, Data_Troop } from "@sigureya/rpgtypes";
+import {
+  FILENAME_ITEMS,
+  FILENAME_ENEMIES,
+  FILENAME_SKILLS,
+  FILENAME_TROOPS,
+  FILENAME_COMMON_EVENTS,
+  FILENAME_WEAPONS,
+  FILANAME_CLASSES,
+  FILENAME_STATES,
+  FILENAME_ACTORS,
+  FILENAME_ARMORS,
+  isDataActor,
+  isDataArmor,
+  isDataEnemy,
+  isDataClass,
+  isDataItem,
+  isDataSkill,
+  isDataState,
+  isDataWeapon,
+} from "@sigureya/rpgtypes";
+
+type FsLib_ReadFile = Pick<typeof FsLib, "readFile">;
+export const callConvertHandler = async <T, R>(
+  fsLib: FsLib_ReadFile,
+  pathLib: typeof PathLib,
+  basePath: string,
+  fileName: string,
+  validateFn: (data: unknown) => data is T,
+  ConvertFn?: (list: T[], fileName: string) => R
+): Promise<R | undefined> => {
+  if (!ConvertFn) {
+    return undefined;
+  }
+  try {
+    const list: T[] = await readDataFile(
+      fsLib,
+      pathLib,
+      basePath,
+      fileName,
+      validateFn
+    );
+    return ConvertFn(list, fileName);
+  } catch {
+    return undefined;
+  }
+};
+
+export const readDataFile = async <T>(
+  fsLib: FsLib_ReadFile,
+  pathLib: Pick<typeof PathLib, "resolve" | "sep">,
+  basePath: string,
+  fileName: string,
+  validateFn: (data: unknown) => data is T
+): Promise<T[]> => {
+  const path: string = ensureDataPath(pathLib, basePath, fileName);
+  const jsonText: string = await fsLib.readFile(path, "utf-8");
+  const obj = JSON.parse(jsonText);
+  if (!Array.isArray(obj)) {
+    throw new Error(`Invalid data format in ${fileName}`);
+  }
+  return obj.filter((item) => validateFn(item));
+};
+
+export const dispatchHandlers = async <T>(
+  fsLib: FsLib_ReadFile,
+  pathLib: typeof PathLib,
+  basePath: string,
+  dataMapper: Partial<DataMapper<T>>
+): Promise<Record<keyof DataMapper<T>, T | undefined>> => {
+  return {
+    actor: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_ACTORS,
+      isDataActor,
+      dataMapper.actor
+    ),
+    armor: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_ARMORS,
+      isDataArmor,
+      dataMapper.armor
+    ),
+    enemy: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_ENEMIES,
+      isDataEnemy,
+      dataMapper.enemy
+    ),
+    class: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILANAME_CLASSES,
+      isDataClass,
+      dataMapper.class
+    ),
+    item: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_ITEMS,
+      isDataItem,
+      dataMapper.item
+    ),
+    skill: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_SKILLS,
+      isDataSkill,
+      dataMapper.skill
+    ),
+    state: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_STATES,
+      isDataState,
+      dataMapper.state
+    ),
+    weapon: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_WEAPONS,
+      isDataWeapon,
+      dataMapper.weapon
+    ),
+    troop: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_TROOPS,
+      (data): data is Data_Troop => !!data, // troopのテストは未実装なので、仮でtrueを返す
+      dataMapper.troop
+    ),
+    commonEvent: await callConvertHandler(
+      fsLib,
+      pathLib,
+      basePath,
+      FILENAME_COMMON_EVENTS,
+      (data): data is Data_CommonEvent => !!data, // commonEventのテストは未実装なので、仮でtrueを返す
+      dataMapper.commonEvent
+    ),
+  };
+};
---
+++ src/libs/folder3/dataDispatch/dataDsipatch.test.ts
@@ -0,0 +1,134 @@
+import { expect, describe, test, vi } from "vitest";
+import type FsLib from "node:fs/promises";
+import type {
+  Data_Actor,
+  Data_Armor,
+  Data_Class,
+  Data_Enemy,
+  Data_Item,
+  Data_Skill,
+  Data_State,
+  Data_Weapon,
+} from "@sigureya/rpgtypes";
+import {
+  isDataActor,
+  makeActor,
+  isDataArmor,
+  makeArmor,
+  isDataClass,
+  isDataEnemy,
+  isDataItem,
+  isDataSkill,
+  isDataState,
+  isDataWeapon,
+  makeClass,
+  makeEnemy,
+  makeItem,
+  makeSkill,
+  makeState,
+  makeWeapon,
+} from "@sigureya/rpgtypes";
+import { readDataFile } from "./dataDispatch";
+
+const makeMockFs = (func: (filepath: string) => Promise<string>) => {
+  return {
+    readFile: vi.fn(func),
+  } as unknown as Pick<typeof FsLib, "readFile">;
+};
+const makeMockPath = (path: string) => ({
+  sep: "/" as const,
+  resolve: vi.fn(() => path),
+});
+
+describe("readDataFile", () => {
+  test("should read file and parse JSON", async () => {
+    const MockActor: string = JSON.stringify([null, makeActor()]);
+    const mockFs = makeMockFs(async () => MockActor);
+    const mockPath = makeMockPath("test/test.json");
+
+    const result = await readDataFile(
+      mockFs,
+      mockPath,
+      "base",
+      "test.json",
+      (x) => isDataActor(x)
+    );
+    expect(result).toEqual([makeActor()]);
+  });
+
+  test("should return empty array for invalid JSON", async () => {
+    const mockFs = makeMockFs(async () => {
+      return JSON.stringify({ id: 0 });
+    });
+    const mockPath = makeMockPath("dummyPath.json");
+    const mockFuncIs = vi.fn((_: unknown): _ is unknown => false);
+    expect(async () => {
+      await readDataFile(
+        mockFs,
+        mockPath,
+        "base",
+        "test.json",
+        (x): x is unknown => mockFuncIs(x)
+      );
+    }).rejects.toThrow();
+    expect(mockFs.readFile).toHaveBeenCalledWith("dummyPath.json", "utf-8");
+    expect(mockFuncIs).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe("validdateFunction", () => {
+  const validateFunctionTest = <T>(
+    fn: (data: unknown) => data is T,
+    data: T
+  ) => {
+    test("should return true for valid data", () => {
+      expect(fn(data)).toBe(true);
+    });
+    test("should return false for invalid object", () => {
+      expect(fn({})).toBe(false);
+    });
+    test("should return false for null", () => {
+      expect(fn(null)).toBe(false);
+    });
+  };
+
+  describe("isDataActor", () => {
+    const actor: Data_Actor = makeActor();
+    validateFunctionTest(isDataActor, actor);
+  });
+
+  describe("isDataArmor", () => {
+    const armor: Data_Armor = makeArmor();
+    validateFunctionTest(isDataArmor, armor);
+  });
+
+  describe("isDataClass", () => {
+    const data: Data_Class = makeClass();
+    validateFunctionTest(isDataClass, data);
+  });
+
+  describe("isDataEnemy", () => {
+    const data: Data_Enemy = makeEnemy();
+    validateFunctionTest(isDataEnemy, data);
+  });
+
+  describe("isDataItem", () => {
+    const data: Data_Item = makeItem();
+    validateFunctionTest(isDataItem, data);
+  });
+
+  describe("isDataSkill", () => {
+    const data: Data_Skill = makeSkill();
+    validateFunctionTest(isDataSkill, data);
+  });
+
+  describe("isDataState", () => {
+    const data: Data_State = makeState();
+    validateFunctionTest(isDataState, data);
+  });
+
+  describe("isDataWeapon", () => {
+    const data: Data_Weapon = makeWeapon();
+    validateFunctionTest(isDataWeapon, data);
+  });
+});
