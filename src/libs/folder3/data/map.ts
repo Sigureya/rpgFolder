aa93d850076cfc7f56f33b7fd50cea858b9d5372
Sigureya <sigureya1208@gmail.com>
2025-05-08 12:12:24 +0900

    mapInfosだけ先行で実装。マップ本体はこの後

---
+++ src/libs/folder3/data/map.ts
@@ -0,0 +1,42 @@
+import {
+  FILENAME_MAP_INFOS,
+  isDataMapInfo,
+  type Data_MapInfo,
+} from "@sigureya/rpgtypes";
+import type {
+  FsLib_ReadFile,
+  FsLib_WriteFile,
+  PathLib_Resolve,
+} from "./detail/libTypes";
+import {
+  ensureDataPath,
+  readRmmzDataListFile,
+  writeDataFile,
+} from "./detail/indentifideItems";
+
+export const ensureMapInfoDataPath = (
+  pathLib: PathLib_Resolve,
+  basePath: string
+): string => {
+  return ensureDataPath(pathLib, basePath, FILENAME_MAP_INFOS);
+};
+
+export const writeRmmzMapInfoData = (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_WriteFile,
+  basePath: string,
+  mapInfos: Data_MapInfo[],
+  space: number = 2
+): Promise<void> => {
+  const path: string = ensureMapInfoDataPath(pathLib, basePath);
+  return writeDataFile(fsLib, path, mapInfos, space);
+};
+
+export const readRmmzMapInfoData = async (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_ReadFile,
+  basePath: string
+): Promise<Data_MapInfo[]> => {
+  const path: string = ensureMapInfoDataPath(pathLib, basePath);
+  return readRmmzDataListFile<Data_MapInfo>(fsLib, path, isDataMapInfo);
+};
