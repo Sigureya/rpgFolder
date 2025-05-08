3ecab61556b1c257ef9dc8e41b504964ec897ff1
Sigureya <sigureya1208@gmail.com>
2025-05-08 12:11:40 +0900

    システム関連のデータ読み書き。ここだけobjectなので他の型と微妙に実装が違う

---
+++ src/libs/folder3/data/system.ts
@@ -0,0 +1,46 @@
+import type { Data_System } from "@sigureya/rpgtypes";
+import { FILENAME_SYSTEM } from "@sigureya/rpgtypes";
+import type {
+  FsLib_ReadFile,
+  FsLib_WriteFile,
+  PathLib_Resolve,
+} from "./detail/libTypes";
+import { ensureDataPath } from "./detail/indentifideItems";
+
+// 仮置き。型が複雑なので実装は後回し
+const isDataSystem = (data: unknown): data is Data_System => {
+  return true;
+};
+
+export const ensureSystemDataPath = (
+  pathLib: PathLib_Resolve,
+  basePath: string
+): string => {
+  return ensureDataPath(pathLib, basePath, FILENAME_SYSTEM);
+};
+
+export const writeRmmzSystemData = (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_WriteFile,
+  basePath: string,
+  system: Data_System,
+  space: number = 2
+): Promise<void> => {
+  const path: string = ensureSystemDataPath(pathLib, basePath);
+  const text = JSON.stringify(system, null, space);
+  return fsLib.writeFile(path, text, "utf8");
+};
+
+export const readRmmzSystemData = async (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_ReadFile,
+  basePath: string
+): Promise<Data_System> => {
+  const path: string = ensureSystemDataPath(pathLib, basePath);
+  const text = await fsLib.readFile(path, "utf8");
+  const data = JSON.parse(text);
+  if (!isDataSystem(data)) {
+    throw new Error(`Invalid system data: ${path}`);
+  }
+  return data;
+};
