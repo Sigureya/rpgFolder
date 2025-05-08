8c4443eb83906dab6ecf8a4da600daa08b8366aa
Sigureya <sigureya1208@gmail.com>
2025-05-08 11:41:48 +0900

    各データ型に対応した関数を追加

---
+++ src/libs/folder3/dataDispatch/skill.ts
@@ -0,0 +1,42 @@
+import {
+  FILENAME_SKILLS,
+  isDataSkill,
+  type Data_Skill,
+} from "@sigureya/rpgtypes";
+import type {
+  FsLib_ReadFile,
+  FsLib_WriteFile,
+  PathLib_Resolve,
+} from "./detail/libTypes";
+import {
+  ensureDataPath,
+  readRmmzDataListFile,
+  writeDataFile,
+} from "./detail/indentifideItems";
+
+export const ensureSkillDataPath = (
+  pathLib: PathLib_Resolve,
+  basePath: string
+): string => {
+  return ensureDataPath(pathLib, basePath, FILENAME_SKILLS);
+};
+
+export const writeRmmzSkillData = (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_WriteFile,
+  basePath: string,
+  skills: Data_Skill[],
+  space: number = 2
+): Promise<void> => {
+  const path: string = ensureSkillDataPath(pathLib, basePath);
+  return writeDataFile(fsLib, path, skills, space);
+};
+
+export const readRmmzSkillData = async (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_ReadFile,
+  basePath: string
+): Promise<Data_Skill[]> => {
+  const path: string = ensureSkillDataPath(pathLib, basePath);
+  return readRmmzDataListFile<Data_Skill>(fsLib, path, isDataSkill);
+};
---
+++ src/libs/folder3/dataDispatch/state.ts
@@ -0,0 +1,42 @@
+import {
+  FILENAME_STATES,
+  isDataState,
+  type Data_State,
+} from "@sigureya/rpgtypes";
+import type {
+  FsLib_ReadFile,
+  FsLib_WriteFile,
+  PathLib_Resolve,
+} from "./detail/libTypes";
+import {
+  ensureDataPath,
+  readRmmzDataListFile,
+  writeDataFile,
+} from "./detail/indentifideItems";
+
+export const ensureStateDataPath = (
+  pathLib: PathLib_Resolve,
+  basePath: string
+): string => {
+  return ensureDataPath(pathLib, basePath, FILENAME_STATES);
+};
+
+export const writeRmmzStateData = (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_WriteFile,
+  basePath: string,
+  states: Data_State[],
+  space: number = 2
+): Promise<void> => {
+  const path: string = ensureStateDataPath(pathLib, basePath);
+  return writeDataFile(fsLib, path, states, space);
+};
+
+export const readRmmzStateData = async (
+  pathLib: PathLib_Resolve,
+  fsLib: FsLib_ReadFile,
+  basePath: string
+): Promise<Data_State[]> => {
+  const path: string = ensureStateDataPath(pathLib, basePath);
+  return readRmmzDataListFile<Data_State>(fsLib, path, isDataState);
+};
