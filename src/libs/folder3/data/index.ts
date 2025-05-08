a369d1927f42e48485579e6eaf32288b6e7f18c2
Sigureya <sigureya1208@gmail.com>
2025-05-09 00:42:48 +0900

    コモンイベントの読み込みを修正。ほぼこれで完成形

--- src/libs/folder3/data/index.ts
+++ src/libs/folder3/data/index.ts
@@ -7,3 +7,4 @@ export * from "./skill";
 export * from "./state";
 export * from "./weapon";
 export * from "./troop";
+export * from "./commonEvent";
--- src/libs/folder3/dataDispatch.ts
+++ src/libs/folder3/dataDispatch.ts
@@ -1,58 +1,32 @@
 import type PathLib from "node:path";
 import type FsLib from "node:fs/promises";
 import type { DataMapper } from "./data/types";
-import type { Data_CommonEvent, Data_Troop } from "@sigureya/rpgtypes";
 import {
-  FILENAME_TROOPS,
-  FILENAME_COMMON_EVENTS,
   SRC_DATA_ACTOR,
   SRC_DATA_ARMOR,
   SRC_DATA_CLASS,
+  SRC_DATA_COMMON_EVNET,
+  SRC_DATA_ENEMY,
   SRC_DATA_ITEMS,
   SRC_DATA_SKILL,
-  SRC_DATA_ENEMY,
-  SRC_DATA_WEAPON,
   SRC_DATA_STATE,
+  SRC_DATA_TROOP,
+  SRC_DATA_WEAPON,
 } from "@sigureya/rpgtypes";
-import { readDataFile } from "./data/detail/detail";
 import {
   readRmmzActorData,
   readRmmzArmorData,
   readRmmzClassData,
+  readRmmzCommonEventData,
   readRmmzEnemyData,
   readRmmzItemData,
   readRmmzSkillData,
   readRmmzStateData,
+  readRmmzTroopData,
   readRmmzWeaponData,
 } from "./data";
 type FsLib_ReadFile = Pick<typeof FsLib, "readFile">;
 type PathLib_Resolve = Pick<typeof PathLib, "resolve" | "sep">;
-
-export const callConvertHandler = async <T, R>(
-  fsLib: FsLib_ReadFile,
-  pathLib: PathLib_Resolve,
-  basePath: string,
-  fileName: string,
-  validateFn: (data: unknown) => data is T,
-  ConvertFn?: (list: T[], fileName: string) => R
-): Promise<R | undefined> => {
-  if (!ConvertFn) {
-    return undefined;
-  }
-  try {
-    const list: T[] = await readDataFile(
-      fsLib,
-      pathLib,
-      basePath,
-      fileName,
-      validateFn
-    );
-    return ConvertFn(list, fileName);
-  } catch {
-    // 例外発生時は全部undefinedにする。詳細なエラーチェックは後で作る
-    return undefined;
-  }
-};
 
 export const dispatchHandlers = async <T>(
   fsLib: FsLib_ReadFile,
@@ -113,21 +87,17 @@ export const dispatchHandlers = async <T>(
         )
       : undefined,
 
-    troop: await callConvertHandler(
-      fsLib,
-      pathLib,
-      basePath,
-      FILENAME_TROOPS,
-      (data): data is Data_Troop => !!data, // troopのテストは未実装なので、仮でtrueを返す
-      dataMapper.troop
-    ),
-    commonEvent: await callConvertHandler(
-      fsLib,
-      pathLib,
-      basePath,
-      FILENAME_COMMON_EVENTS,
-      (data): data is Data_CommonEvent => !!data, // commonEventのテストは未実装なので、仮でtrueを返す
-      dataMapper.commonEvent
-    ),
+    troop: dataMapper.troop
+      ? dataMapper.troop(
+          await readRmmzTroopData(pathLib, fsLib, basePath),
+          SRC_DATA_TROOP
+        )
+      : undefined,
+    commonEvent: dataMapper.commonEvent
+      ? dataMapper.commonEvent(
+          await readRmmzCommonEventData(pathLib, fsLib, basePath),
+          SRC_DATA_COMMON_EVNET
+        )
+      : undefined,
   };
 };
