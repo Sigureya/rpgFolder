e408a1a2b16b0a51aa06ad25fdb18e1e6b04fd98
Sigureya <sigureya1208@gmail.com>
2025-05-10 01:37:35 +0900

    マップデータ本体を読み込む関数

--- src/libs/folder3/data/map.ts
+++ src/libs/folder3/data/map.ts
@@ -1,38 +1,90 @@
+import type { Data_Map, MapFileInfo } from "@sigureya/rpgtypes";
 import {
-  FILENAME_MAP_INFOS,
   isDataMapInfo,
+  makeMapData,
   type Data_MapInfo,
 } from "@sigureya/rpgtypes";
-import type {
-  FsLib_ReadFile,
-  FsLib_WriteFile,
-  PathLib_Resolve,
-} from "./detail/libTypes";
-import { ensureDataPath, readRmmzDataListFile, writeDataFile } from "./detail";
+import type { FsLib_ReadFile, PathLib_Resolve } from "./detail/libTypes";
+import type FsLib from "node:fs/promises";
+import type { Dirent } from "node:fs";
+import { readRmmzMapInfoData } from "./mapInfo";
+type InfoType = Pick<Data_MapInfo, "name" | "id">;
 
-export const ensureMapInfoDataPath = (
-  pathLib: PathLib_Resolve,
-  basePath: string
-): string => {
-  return ensureDataPath(pathLib, basePath, FILENAME_MAP_INFOS);
+export const isMapFileName = (filename: string) => {
+  return filename.startsWith("Map") && filename.endsWith(".json");
 };
 
-export const writeRmmzMapInfoData = (
+export const listupMapFiles = async (
+  fsLib: Pick<typeof FsLib, "readdir">,
+  resolvedPath: string
+): Promise<Dirent[]> => {
+  return (await fsLib.readdir(resolvedPath, { withFileTypes: true })).filter(
+    (dirent) => dirent.isFile() && isMapFileName(dirent.name)
+  );
+};
+
+const makeMapFileName = (mapId: number) => {
+  return `Map${String(mapId).padStart(3, "0")}.json` as const;
+};
+
+export const resolveMapFilePath = (
   pathLib: PathLib_Resolve,
-  fsLib: FsLib_WriteFile,
   basePath: string,
-  mapInfos: Data_MapInfo[],
-  space: number = 2
-): Promise<void> => {
-  const path: string = ensureMapInfoDataPath(pathLib, basePath);
-  return writeDataFile(fsLib, path, mapInfos, space);
+  mapId: number
+) => {
+  const fileName = makeMapFileName(mapId);
+  const filePath = pathLib.resolve(basePath, fileName);
+  return { filePath, fileName };
 };
 
-export const readRmmzMapInfoData = async (
-  pathLib: PathLib_Resolve,
+export type ResultOfReadMapFile = MapFileInfo<Data_Map | undefined>;
+const validateAndNormalizeMapData = (data: unknown): Data_Map => {
+  // データの不足分を補い、余計なデータを排除する処理
+  return makeMapData(data as {});
+};
+
+export const readMapFileFromInfo = async (
   fsLib: FsLib_ReadFile,
-  basePath: string
-): Promise<Data_MapInfo[]> => {
-  const path: string = ensureMapInfoDataPath(pathLib, basePath);
-  return readRmmzDataListFile<Data_MapInfo>(fsLib, path, isDataMapInfo);
+  pathLib: PathLib_Resolve,
+  basePath: string,
+  info: InfoType
+): Promise<ResultOfReadMapFile> => {
+  const { filePath, fileName } = resolveMapFilePath(pathLib, basePath, info.id);
+
+  try {
+    const text = await fsLib.readFile(filePath, "utf-8");
+    const mapData: Data_Map = validateAndNormalizeMapData(JSON.parse(text));
+    return {
+      filename: fileName,
+      map: mapData,
+      editingName: info.name,
+    };
+  } catch {
+    return {
+      filename: fileName,
+      editingName: info.name,
+      map: undefined,
+    };
+  }
+};
+
+export const mappingAllMapFiles = async <T>(
+  fsLib: FsLib_ReadFile,
+  pathLib: PathLib_Resolve,
+  basePath: string,
+  fn: (data: ResultOfReadMapFile) => T,
+  infos?: ReadonlyArray<Data_MapInfo | null>
+): Promise<Promise<T>[]> => {
+  const list = infos ?? (await readRmmzMapInfoData(pathLib, fsLib, basePath));
+  return list
+    .filter((item) => !!item && isDataMapInfo(item))
+    .map(async (info: Data_MapInfo) => {
+      const mapFileInfo = await readMapFileFromInfo(
+        fsLib,
+        pathLib,
+        basePath,
+        info
+      );
+      return fn(mapFileInfo);
+    });
 };
