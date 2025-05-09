0418535c59bcddabd6ac853bcd433df109e340b5
Sigureya <sigureya1208@gmail.com>
2025-05-09 10:16:24 +0900

    複数ファイルを一気に書き込むための関数

---
+++ src/libs/folder3/multiWrite.ts
@@ -0,0 +1,38 @@
+import {
+  writeRmmzActorData,
+  writeRmmzEnemyData,
+  writeRmmzArmorData,
+  writeRmmzWeaponData,
+  writeRmmzClassData,
+  writeRmmzItemData,
+  writeRmmzSkillData,
+  writeRmmzStateData,
+} from "./data";
+import type { FsLib_WriteFile, PathLib_Resolve } from "./data/detail/libTypes";
+import type { DataTable } from "./data/types";
+
+const ggg = (fm: () => Promise<void>) => {
+  try {
+    return fm();
+  } catch {
+    return Promise.resolve();
+  }
+};
+
+export const writeDataFiles = async (
+  fsLib: FsLib_WriteFile,
+  pathLib: PathLib_Resolve,
+  basePath: string,
+  data: DataTable
+) => {
+  return Promise.all([
+    ggg(() => writeRmmzActorData(pathLib, fsLib, basePath, data.actors)),
+    ggg(() => writeRmmzEnemyData(pathLib, fsLib, basePath, data.enemies)),
+    ggg(() => writeRmmzArmorData(pathLib, fsLib, basePath, data.armors)),
+    ggg(() => writeRmmzWeaponData(pathLib, fsLib, basePath, data.weapons)),
+    ggg(() => writeRmmzClassData(pathLib, fsLib, basePath, data.classes)),
+    ggg(() => writeRmmzItemData(pathLib, fsLib, basePath, data.items)),
+    ggg(() => writeRmmzSkillData(pathLib, fsLib, basePath, data.skills)),
+    ggg(() => writeRmmzStateData(pathLib, fsLib, basePath, data.states)),
+  ]);
+};
