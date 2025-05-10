4ab964667ea14477423bffd866a3645b783ebf39
Sigureya <sigureya1208@gmail.com>
2025-05-10 19:13:11 +0900

    古いファイルを削除。nodeのファイル系関数を扱っていたが、今は不要になった

--- src/libs/nodeLib/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./types";
-export * from "./path";
-export * from "./folder";
-export * from "./file";
--- src/libs/nodeLib/types/libTypes.ts
+++
@@ -1,7 +0,0 @@
-import type fsp from "node:fs/promises";
-import type pathLib from "node:path";
-
-export interface NodeLibs {
-  fs: typeof fsp;
-  path: typeof pathLib;
-}
