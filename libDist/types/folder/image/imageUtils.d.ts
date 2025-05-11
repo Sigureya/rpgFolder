213f7ba262b50254ee0536d09ea9af924d4edd0c
Sigureya <sigureya1208@gmail.com>
2025-05-11 12:52:57 +0900

    未使用に追記削除

--- libDist/types/folder/image/imageUtils.d.ts
+++
@@ -1,5 +0,0 @@
-import { Dirent } from 'node:fs';
-import { default as PathLib } from 'node:path';
-import { default as FsLib } from 'node:fs/promises';
-export declare const imageFiles: (pathLib: Pick<typeof PathLib, "resolve">, fsLib: Pick<typeof FsLib, "readdir">, basePath: string, subFolder: string) => Promise<Dirent[]>;
-export declare const imageFolderPath: (pathLib: Pick<typeof PathLib, "resolve">, basePath: string, subFolder: string) => string;
